6f41715ea8b09057e4c1882e7f9d49060a46bb0f
Intizar-T <intizar.tashov22@gmail.com>
2022-09-08 17:03:15 +0900

    buttons and dropdowns implemented

--- src/Components/Main/Header.tsx
+++ src/Components/Main/Header.tsx
@@ -6,37 +6,164 @@ import BaseInput from "../Base/Input";
 
 function Header() {
   const [showLanguageDropdown, setLanguageDropdown] = useState(false);
+  const [showRakingDropdown, setRankingDropdown] = useState(false);
+  const [showMoreDropdown, setMoreDropdown] = useState(false);
 
+  const Menus = ["Communities", "Competitions", "Learning"];
   return (
     <Navbar className="mx-auto w-full">
-      <div className="mx-auto flex flex-row flex-wrap items-center">
+      <div className="mx-auto flex flex-row flex-wrap items-center justify-between">
+        {/* Menus (DeepMeta, Communities, Competitions, Learning, Ranking, More) */}
         <div className="flex flex-[0_0_50%] justify-start items-center">
-          <BaseButton>
-            <Typography variant="h5" className="mr-12" color="blue">
-              DeepMeta
+          {/* Logo */}
+          <BaseButton className="text-transform: normal-case">
+            <Typography variant="h5" className="" color="blue">
+              AutoDeep
             </Typography>
           </BaseButton>
 
-          <Typography color="gray" className="mr-4 hover:bg-orange-500">
-            Communities
-          </Typography>
-          <Typography color="gray" className="mr-4">
-            Competitions
-          </Typography>
-          <Typography color="gray" className="mr-4">
-            Learning
-          </Typography>
-          <Typography color="gray" className="mr-4">
-            Ranking
-          </Typography>
-          <Typography color="gray" className="mr-4">
-            More
-          </Typography>
+          {/* Communities, Competetions, and Learning buttons */}
+          {Menus.map((menu) => {
+            return (
+              <BaseButton className="text-transform: normal-case">
+                <Typography variant="h6" color="gray" className="">
+                  {menu}
+                </Typography>
+              </BaseButton>
+            );
+          })}
+
+          {/* Ranking button and dropdown */}
+          <BaseButton
+            className="text-transform: normal-case"
+            func={() => {
+              setRankingDropdown(true);
+            }}
+          >
+            <Typography variant="h6" color="gray" className="">
+              Ranking
+            </Typography>
+          </BaseButton>
+          {showRakingDropdown && (
+            <BaseDropdown
+              className="relative z-[9999]"
+              onClose={() => {
+                setRankingDropdown(false);
+              }}
+              id="rankingDropdown"
+            >
+              <div className="absolute right-0 z-10 mt-6 w-36 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ">
+                <ul className="py-1 divide-gray-100 divide-y" role="none">
+                  <li>
+                    <BaseButton
+                      ripple={false}
+                      className="text-black shadow-none"
+                    >
+                      <Typography className="text-center text-sm">
+                        Ranking
+                      </Typography>
+                    </BaseButton>
+                  </li>
+                  <li>
+                    <BaseButton
+                      ripple={false}
+                      className="text-black shadow-none"
+                    >
+                      <Typography className="text-center text-sm">
+                        Tier System
+                      </Typography>
+                    </BaseButton>
+                  </li>
+                  <li>
+                    <BaseButton
+                      ripple={false}
+                      className="text-black shadow-none"
+                    >
+                      <Typography className="text-center text-sm">
+                        Interview
+                      </Typography>
+                    </BaseButton>
+                  </li>
+                </ul>
+              </div>
+            </BaseDropdown>
+          )}
+
+          {/* More Button and dropdown */}
+          <BaseButton
+            className="text-transform: normal-case"
+            func={() => {
+              setMoreDropdown(true);
+            }}
+          >
+            <Typography variant="h6" color="gray" className="">
+              More
+            </Typography>
+          </BaseButton>
+          {showMoreDropdown && (
+            <BaseDropdown
+              className="relative z-[9999]"
+              onClose={() => {
+                setMoreDropdown(false);
+              }}
+              id="moreDropdown"
+            >
+              <div className="absolute right-0 z-10 mt-6 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ">
+                <ul className="py-1 divide-gray-100 divide-y" role="none">
+                  <li>
+                    <BaseButton
+                      ripple={false}
+                      className="text-black shadow-none"
+                    >
+                      <Typography className="text-center text-sm">
+                        Notice
+                      </Typography>
+                    </BaseButton>
+                  </li>
+                  <li>
+                    <BaseButton
+                      ripple={false}
+                      className="text-black shadow-none"
+                    >
+                      <Typography className="text-center text-sm">
+                        FAQ
+                      </Typography>
+                    </BaseButton>
+                  </li>
+                  <li>
+                    <BaseButton
+                      ripple={false}
+                      className="text-black shadow-none"
+                    >
+                      <Typography className="text-sm">
+                        How to Participate
+                      </Typography>
+                    </BaseButton>
+                  </li>
+                  <li>
+                    <BaseButton
+                      ripple={false}
+                      className="text-black shadow-none"
+                    >
+                      <Typography className="text-center text-sm">
+                        About Us
+                      </Typography>
+                    </BaseButton>
+                  </li>
+                </ul>
+              </div>
+            </BaseDropdown>
+          )}
         </div>
-        <div className="flex flex-[0_0_50%] justify-end items-center">
+
+        {/* Search, Lang, Sing in and up */}
+        <div className="flex flex-[0_0_40%] justify-end items-center">
+          {/* Search input */}
           <div className="w-52 mr-4">
             <BaseInput label="Search" iconName="fas fa-heart" />
           </div>
+
+          {/* Language icon and dropdown */}
           <div className="">
             <BaseButton
               func={() => {
@@ -65,7 +192,9 @@ function Header() {
                         ripple={false}
                         className="text-black shadow-none"
                       >
-                        English
+                        <Typography className="text-center text-sm">
+                          English
+                        </Typography>
                       </BaseButton>
                     </li>
                     <li>
@@ -73,7 +202,9 @@ function Header() {
                         ripple={false}
                         className="text-black shadow-none"
                       >
-                        Korean
+                        <Typography className="text-center text-sm">
+                          Korean
+                        </Typography>
                       </BaseButton>
                     </li>
                   </ul>
@@ -81,6 +212,8 @@ function Header() {
               </BaseDropdown>
             )}
           </div>
+
+          {/* Sign in and up */}
           <BaseButton className="text-gray-600 text-md">Sign in</BaseButton>
           <BaseButton className="text-orange-300 text-md">Sign up</BaseButton>
         </div>
