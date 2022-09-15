d79cbb6d7da955b2b05ae5bee7b267d05dcfa43a
Intizar-T <intizar.tashov22@gmail.com>
2022-09-15 16:35:05 +0900

    initial attempt (failing miserably)

--- src/Views/Pages/Competitions.tsx
+++ src/Views/Pages/Competitions.tsx
@@ -7,6 +7,18 @@ import { CarouselData } from "../../Models/Carousel/Data";
 import { WindowSizeData } from "../../Models/WindowSize/Data";
 import { useResizeDetector } from "react-resize-detector";
 import { Typography } from "@material-tailwind/react";
+import {
+  competitionNames,
+  competitionThemes,
+} from "../../Models/Competitions/Data";
+import {
+  listCompetitionNamesLength,
+  gridCompetitionNamesLength,
+  listCompetitionThemesLength,
+  dataLength,
+  listViewImageSize,
+  gridViewImageSize,
+} from "../../Models/Competitions/Resizing";
 
 interface CompetitionsProps {
   competitionNumber?: number;
@@ -15,106 +27,106 @@ interface CompetitionsProps {
 
 function Competitions({ competitionNumber, gridView }: CompetitionsProps) {
   const { width, height, ref } = useResizeDetector();
-
-  const competitionNames = [
-    "Model Training for Classifying Varieties of Irises",
-    "Predicting Fuel Efficiency of Cars",
-    "Recognizing Handwritten Numbers",
-    "Predicting Baseball Pitch Types",
-    "Transfer Learning MNIST Model",
-    "Training the Wine Classification Model",
-  ];
-  const competitionNamesLength = width
-    ? width < WindowSizeData["xs"]
-      ? 0.05
-      : width < WindowSizeData["md"]
-      ? 0.06
-      : 0.075
-    : 0.05;
-  const competitionThemes = "Algorithms | Vision | Neural Network | Regression";
-  const competitionThemesLength = width
-    ? width < WindowSizeData["xs"]
-      ? 0.06
-      : width < WindowSizeData["md"]
-      ? 0.075
-      : 0.1
-    : 0.06;
-  const data = competitionNumber
-    ? CarouselData.slice(0, competitionNumber)
-    : CarouselData;
-
-  const imageSize =
-    width &&
-    (width < WindowSizeData["xsm"]
-      ? "w-16 h-16"
-      : width < WindowSizeData["xmd"]
-      ? "w-20 h-20"
-      : "w-24 h-24");
+  const listNamesLength = listCompetitionNamesLength(width);
+  const gridNamesLength = gridCompetitionNamesLength(width);
+  if (width)
+    console.log(
+      gridNamesLength +
+        ", " +
+        width +
+        ", " +
+        competitionNames[0].substring(0, gridNamesLength)
+    );
+  const listThemesLength = listCompetitionThemesLength(width);
+  const data = dataLength(competitionNumber, CarouselData);
+  const listImageSize = listViewImageSize(width);
+  const gridImageSize = gridViewImageSize(width);
 
   return (
     <div
       ref={ref}
       className={`w-full h-full flex flex-${
         gridView ? "row" : "col"
-      } flex-wrap justify-center`}
+      } flex-wrap justify-start`}
     >
       {gridView
         ? data.map((imgObj: { image: string }, index: number) => {
             return (
-              <BaseButton className="text-transform: normal-case">
-                <BaseCard className="h-72 w-72 sm:h-96 sm:w-96 mr-2">
-                  <BaseCardHeader color="blue" className="mt-2 w-50 h-50">
-                    <img
-                      src={imgObj.image}
-                      alt="dummy images"
-                      key={index}
-                      className="h-full w-full block object-cover"
-                    />
+              <BaseButton
+                className={`text-transform: normal-case w-${
+                  width &&
+                  (width < WindowSizeData["sm"]
+                    ? "1/2"
+                    : width < WindowSizeData["md"]
+                    ? "1/3"
+                    : "1/4")
+                }`}
+              >
+                <BaseCard className="">
+                  <BaseCardHeader color="blue" className="mt-2">
+                    <div className={gridImageSize ? gridImageSize : ""}>
+                      <img
+                        src={imgObj.image}
+                        alt="dummy images"
+                        key={index}
+                        className="h-full w-full block object-cover"
+                      />
+                    </div>
                   </BaseCardHeader>
                   <BaseCardBody>
                     <Typography variant="h6">
-                      {competitionNames[index].length > 30
-                        ? competitionNames[index].substring(0, 30) + "..."
-                        : competitionNames[index]}
+                      {competitionNames[index].substring(0, gridNamesLength)}
                     </Typography>
                     <Typography className="text-sm">
-                      {competitionThemes}
+                      {competitionThemes[index].substring(0, gridNamesLength)}
                     </Typography>
-                    <div className="flex flex-row justify-center items-center">
-                      <img
-                        className="h-6 w-15 mr-2"
-                        src="https://dacon.s3.ap-northeast-2.amazonaws.com/competition/235978/logo1.jpeg"
-                        alt=""
-                      />
-                      <img
-                        className="h-6 w-15 mr-2"
-                        src="https://dacon.s3.ap-northeast-2.amazonaws.com/competition/235953/logo1.jpeg"
-                        alt=""
-                      />
-                      <img
-                        className="h-6 w-15 mr-2"
-                        src="https://dacon.s3.ap-northeast-2.amazonaws.com/competition/235927/logo1.jpeg"
-                        alt=""
-                      />
-                    </div>
+                    {width && width > WindowSizeData["md"] && (
+                      <div className="flex flex-row justify-center items-center">
+                        <img
+                          className="h-6 w-15 mr-2"
+                          src="https://dacon.s3.ap-northeast-2.amazonaws.com/competition/235978/logo1.jpeg"
+                          alt=""
+                        />
+                        <img
+                          className="h-6 w-15 mr-2"
+                          src="https://dacon.s3.ap-northeast-2.amazonaws.com/competition/235953/logo1.jpeg"
+                          alt=""
+                        />
+                        <img
+                          className="h-6 w-15 mr-2"
+                          src="https://dacon.s3.ap-northeast-2.amazonaws.com/competition/235927/logo1.jpeg"
+                          alt=""
+                        />
+                      </div>
+                    )}
                   </BaseCardBody>
                   <BaseCardFooter
                     divider={true}
-                    className="h-14 flex flex-row justify-between items-center"
+                    className={`h-14 flex flex-row justify-${
+                      width && width > WindowSizeData["lg"]
+                        ? "between"
+                        : "center"
+                    } items-center`}
                   >
                     <Typography
                       color="green"
-                      className="text-sm flex items-center"
+                      className="text-sm flex items-center mr-1"
                       variant="h6"
                     >
-                      ● Active
+                      {width && width > WindowSizeData["lg"]
+                        ? "● Active"
+                        : "D-10"}
                     </Typography>
                     <Typography className="text-sm flex items-center">
-                      D-10 | 500 people
+                      {width && width > WindowSizeData["lg"]
+                        ? "D-10 | 500 people"
+                        : "● 500 people"}
                     </Typography>
-                    <Typography variant="h6" className="flex items-center">
-                      5M Price
-                    </Typography>
+                    {width && width > WindowSizeData["lg"] && (
+                      <Typography variant="h6" className="flex items-center">
+                        5M Price
+                      </Typography>
+                    )}
                   </BaseCardFooter>
                 </BaseCard>
               </BaseButton>
@@ -127,7 +139,7 @@ function Competitions({ competitionNumber, gridView }: CompetitionsProps) {
                   <BaseCardBody>
                     <div className="flex flex-row justify-between">
                       <div className="flex flex-row justify-start mr-2">
-                        <div className={imageSize ? imageSize : ""}>
+                        <div className={listImageSize ? listImageSize : ""}>
                           <img
                             src={imgObj.image}
                             alt="dummy images"
@@ -146,20 +158,20 @@ function Competitions({ competitionNumber, gridView }: CompetitionsProps) {
                           >
                             {width &&
                             competitionNames[index].length >
-                              Math.floor(competitionNamesLength * width)
+                              Math.floor(listNamesLength * width)
                               ? competitionNames[index].substring(
                                   0,
-                                  Math.floor(competitionNamesLength * width)
+                                  Math.floor(listNamesLength * width)
                                 ) + "..."
                               : competitionNames[index]}
                           </Typography>
                           <Typography className="text-sm flex justify-start">
                             {width &&
                             competitionThemes.length >
-                              Math.floor(competitionThemesLength * width)
+                              Math.floor(listThemesLength * width)
                               ? competitionThemes.substring(
                                   0,
-                                  Math.floor(competitionThemesLength * width)
+                                  Math.floor(listThemesLength * width)
                                 ) + "..."
                               : competitionThemes}
                           </Typography>
