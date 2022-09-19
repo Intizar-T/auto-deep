005a54cb9ec408bb72973ac2a65d598f576166df
Intizar-T <intizar.tashov22@gmail.com>
2022-09-19 15:54:16 +0900

    Competition flyer is ready

--- src/Views/Pages/Competition/Competition.tsx
+++ src/Views/Pages/Competition/Competition.tsx
@@ -1,38 +1,101 @@
 import { Typography } from "@material-tailwind/react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { CarouselData } from "../../../Models/Carousel/Data";
+import { competitionNames } from "../../../Models/Competitions/Data";
+import { competitionThemes } from "../../../Models/Competitions/Data";
+import PaidOutlinedIcon from "@mui/icons-material/PaidOutlined";
+import AccessTimeOutlinedIcon from "@mui/icons-material/AccessTimeOutlined";
+import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined";
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import BaseButton from "../../Base/Button";
+import { WindowSizeData } from "../../../Models/WindowSize/Data";
+import { useResizeDetector } from "react-resize-detector";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 
 function Competition() {
   const params = useParams();
+  const { width, ref } = useResizeDetector();
   const competitionId = params["competitionId"]
     ? parseInt(params["competitionId"])
     : 0;
+  const joinText =
+    width && width > WindowSizeData["md"] ? "Join Competition" : "Join";
+  const mobileView = width && width < WindowSizeData["xsm"];
   return (
-    <div className="w-full h-auto">
+    <div ref={ref} className="w-full h-auto">
       <div
-        className="w-full h-72 flex flex-col items-center overflow-hidden relative p-4"
+        className={`flex flex-${
+          mobileView ? "col" : "row"
+        } justify-between w-full h-${mobileView ? "96" : "72"} p-8`}
         style={{
           backgroundImage: `url("${CarouselData[competitionId].image}")`,
-          display: "block",
-          objectFit: "fill",
         }}
       >
-        <div className="w-full h-full flex flex-col justify-start">
-          <Typography variant="h4" className="text-white mb-2">
-            Competition {competitionId}
+        <div className="flex flex-col justify-center">
+          <Typography
+            variant={width && width < WindowSizeData["xsm"] ? "h5" : "h4"}
+            className="text-black mb-2"
+          >
+            {competitionNames[competitionId]}
           </Typography>
-          <Typography variant="h4" className="text-white mb-2">
-            Competition {competitionId}
+          <Typography variant="h6" className="text-black mb-2">
+            {competitionThemes}
           </Typography>
-          <Typography variant="h4" className="text-white mb-2">
-            Competition {competitionId}
-          </Typography>
-          <Typography variant="h4" className="text-white mb-2">
-            Competition {competitionId}
-          </Typography>
-          <Typography variant="h4" className="text-white mb-2">
-            Competition {competitionId}
-          </Typography>
+          <div className="flex flex-row items-start">
+            <PaidOutlinedIcon className="text-black mr-1" fontSize="medium" />
+            <Typography variant="h6" className="text-black mb-2">
+              Prize: 5 Million Won
+            </Typography>
+          </div>
+          <div className="flex flex-row items-start">
+            <AccessTimeOutlinedIcon
+              className="text-black mr-1"
+              fontSize="medium"
+            />
+            <Typography variant="h6" className="text-black mb-2">
+              2022.01.01 ~ 2025.01.01
+            </Typography>
+          </div>
+          <div className="flex flex-row">
+            <div className="flex flex-row mr-4">
+              <PeopleAltOutlinedIcon
+                className="text-black mr-1"
+                fontSize="medium"
+              />
+              <Typography variant="h6" className="text-black mr-2">
+                200 people
+              </Typography>
+            </div>
+            <div className="flex flex-row">
+              <CalendarMonthOutlinedIcon
+                className="text-black mr-1"
+                fontSize="medium"
+              />
+              <Typography variant="h6" className="text-black mr-2">
+                D-50
+              </Typography>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-end mt-2">
+          <BaseButton
+            variant="outlined"
+            color="white"
+            className={`h-${mobileView ? "10" : "12"} mr-2`}
+          >
+            <ShareOutlinedIcon
+              className="text-white"
+              fontSize={mobileView ? "medium" : "large"}
+            />
+          </BaseButton>
+          <BaseButton
+            func={() => {}}
+            className={`bg-white text-transform: normal-case h-${
+              mobileView ? "10" : "12"
+            }`}
+          >
+            <Typography className="text-black">{joinText}</Typography>
+          </BaseButton>
         </div>
       </div>
     </div>
